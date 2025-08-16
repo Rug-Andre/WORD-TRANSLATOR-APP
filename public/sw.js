d9fa0597ca53ee6a4bf814ac7d3d9c41bb251aca
RUGERINYANGE Andre <rugera2007@gmail.com>
2025-08-16 10:27:04 -1000

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+// On install, cache necessary files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('translater-app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/translater.css',
+        '/language.js',
+        '/main.js',
+        '/images/translater-app-cover.png',
+        // Add any other resources you want to cache
+      ]);
+    })
+  );
+});
+
+// Intercept fetch requests and serve cached assets or fetch from network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
